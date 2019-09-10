cb6a727c18d1102ed320dd80686cc03246297c78
Benoit Vendette <benboxx@outlook.com>
2019-09-10 18:19:07 -0400

    tuesday2

--- README.md
+++ README.md
@@ -7,3 +7,76 @@ cd /c/Projets/RocketElevatorsDigitalPresence
 git add .
 git commit -m
 git push origin master
+
+
+
+
+
+
+$("input[name*='building_type']").change(function(){
+          //alert("The text has been changed.");
+          console.log("selected value", $(this).val());
+          var selected_building_type = $(this).val();
+          if( selected_building_type == "residential") {
+              $("#residential_info_zone").show();
+              $("#commercial_info_zone").hide();
+              $("#corporate_info_zone").hide();
+              $("#hybrid_info_zone").hide();
+
+
+              //question and answer
+$(document).ready(function(){
+   $('#building_type').change(function(){
+       var type = $(this).val();
+       if( type == "residential") {
+           $("#residential").show();
+           $("#commercial,#corporate,#hybrid").hide();
+       }
+       else if( type == "commercial") {
+           $("#commercial").show();
+           $("#residential,#corporate,#hybrid").hide();
+       }
+       else if( type == "corporate") {
+           $("#corporate").show();
+           $("#residential,#commercial,#hybrid").hide();
+       }
+       else if( type == "hybrid") {
+           $("#hybrid").show();
+           $("#residential,#corporate,#commercial").hide();
+       }
+       else if( type == "0") {
+           $("#residential,#commercial,#corporate,#hybrid").hide();
+       }
+   });
+});
+
+
+
+
+
+
+//question and answer
+$(document).ready(function(){
+   $('#building_type').change(function(){
+       var type = $(this).val();
+       if( type == "residential") {
+           $("#residential").show();
+           $("#commercial,#corporate,#hybrid").hide();
+       }
+       else if( type == "commercial") {
+           $("#commercial").show();
+           $("#residential,#corporate,#hybrid").hide();
+       }
+       else if( type == "corporate") {
+           $("#corporate").show();
+           $("#residential,#commercial,#hybrid").hide();
+       }
+       else if( type == "hybrid") {
+           $("#hybrid").show();
+           $("#residential,#corporate,#commercial").hide();
+       }
+       else if( type == "0") {
+           $("#residential,#commercial,#corporate,#hybrid").hide();
+       }
+   });
+});
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,5 +1,91 @@
 //question and answer
 
+function collect_data(building_type){
+
+    var donnees = {};
+
+    if( building_type == "residential") {
+        var nb_appartments = parseInt($('#nb_appartments').val());
+        console.log(nb_appartments);
+        donnees["nb_appartments"] = nb_appartments;
+       
+        var nb_floors = parseInt($('#nb_floors').val());
+        console.log(nb_floors);
+        donnees["nb_floors"] = nb_floors;
+       
+        var nb_basements = parseInt($('#nb_basements').val());
+        console.log(nb_basements);   
+    }
+    
+    if( building_type == "commercial") {
+        var nb_business = parseInt($('#nb_business').val());
+        console.log(nb_business);    
+   
+        var nb_level_building = parseInt($('#nb_level_building').val());
+        console.log(nb_level_building); 
+
+        var nb_level_basement = parseInt($('#nb_level_basement').val());
+        console.log(nb_level_basement); 
+
+        var nb_parking = parseInt($('#nb_parking').val());
+        console.log(nb_parking);
+
+        var nb_elv_shaft = parseInt($('#nb_elv_shaft').val());
+        console.log(nb_elv_shaft);
+        
+    }
+
+    if( building_type == "corporative") {
+        var nb_corpo_business = parseInt($('#nb_corpo_business').val());
+        console.log(nb_corpo_business);  
+
+    if( building_type == "corporative") {
+        var nb_corpo_floors = parseInt($('#nb_corpo_floors').val());
+        console.log(nb_corpo_floors); 
+
+    if( building_type == "corporative") {
+        var nb_corpo_level_basement = parseInt($('#nb_corpo_level_basement').val());
+        console.log(nb_corpo_level_basement);
+
+    if( building_type == "corporative") {
+        var nb_corpo_parking = parseInt($('#nb_corpo_parking').val());
+        console.log(nb_corpo_parking);
+
+    if( building_type == "corporative") {
+        var nb_corpo_max_pers_level = parseInt($('#nb_corpo_max_pers_level').val());
+        console.log(nb_corpo_max_pers_level); 
+
+    }
+
+    if( building_type == "hybride") {
+        var nb_diff_business = parseInt($('#nb_diff_business').val());
+        console.log(nb_diff_business);
+
+    if( building_type == "hybride") {
+        var nb_hyb_floors = parseInt($('#nb_hyb_floors').val());
+        console.log(nb_hyb_floors);
+
+    if( building_type == "hybride") {
+        var nb_hyb_basement = parseInt($('#nb_hyb_basement').val());
+        console.log(nb_hyb_basement);
+
+    if( building_type == "hybride") {
+        var nb_hyb_parking = parseInt($('#nb_hyb_parking').val());
+        console.log(nb_hyb_parking);
+
+    if( building_type == "hybride") {
+        var nb_hyb_max_pers_level = parseInt($('#nb_hyb_max_pers_level').val());
+        console.log(nb_hyb_max_pers_level);
+
+    if( building_type == "hybride") {
+        var nb_hrs_activ_day = parseInt($('#nb_hrs_activ_day').val());
+        console.log(nb_hrs_activ_day);
+
+    }
+
+    return donnees;
+}
+
 
 $(document).ready(function(){
     $('#building_type').change(function(){
@@ -10,7 +96,7 @@ $(document).ready(function(){
             compute_data(building_type, collected_data);
 
             $("#residential").show();
-            // $(".commercial,#corporate,#hybrid").hide();
+            // $(".commercial,#corporate,#hybrid").hide(); 
         }
         else if( building_type == "commercial") {
             $("#commercial").show();
@@ -29,120 +115,10 @@ $(document).ready(function(){
         }
     });
 
-    function collect_data(building_type){
-        if( building_type == "residential") {
-            var nb_appartments = $('#nb_appartments').val();
-            console.log(nb_appartments);
-        }
-        
-        if( building_type == "residential") {
-            var nb_floors = $('#nb_floors').val();
-            console.log(nb_floors);
-        }
-        
-        if( building_type == "residential") {
-            var nb_basements = $('#nb_basements').val();
-            console.log(nb_basements);    
-        }   
-             
-        if( building_type == "commercial") {
-            var nb_business = $('#nb_business').val();
-            console.log(nb_business);    
-        }
+     function compute_data(building_type, collected_data){
 
-        if( building_type == "commercial") {
-            var nb_level_building = $('#nb_level_building').val();
-            console.log(nb_level_building); 
+        console.log("compute_data");
 
-        }
-
-        if( building_type == "commercial") {
-            var nb_level_basement = $('#nb_level_basement').val();
-            console.log(nb_level_basement); 
-
-        }
-
-        if( building_type == "commercial") {
-            var nb_parking = $('#nb_parking').val();
-            console.log(nb_parking);
-
-        }
-
-        if( building_type == "commercial") {
-            var nb_elv_shaft = $('#nb_elv_shaft').val();
-            console.log(nb_elv_shaft);
-            
-        }
-
-        if( building_type == "corporative") {
-            var nb_corpo_business = $('#nb_corpo_business').val();
-            console.log(nb_corpo_business);  
-
-        }
-
-        if( building_type == "corporative") {
-            var nb_corpo_floors = $('#nb_corpo_floors').val();
-            console.log(nb_corpo_floors); 
-
-        }
-
-        if( building_type == "corporative") {
-            var nb_corpo_level_basement = $('#nb_corpo_level_basement').val();
-            console.log(nb_corpo_level_basement);
-
-        }
-            
-        if( building_type == "corporative") {
-            var nb_corpo_parking = $('#nb_corpo_parking').val();
-            console.log(nb_corpo_parking);
-
-        }
-            
-        if( building_type == "corporative") {
-            var nb_corpo_max_pers_level = $('#nb_corpo_max_pers_level').val();
-            console.log(nb_corpo_max_pers_level); 
-
-        }
-
-        if( building_type == "hybride") {
-            var nb_diff_business = $('#nb_diff_business').val();
-            console.log(nb_diff_business);
-
-        }
-
-        if( building_type == "hybride") {
-            var nb_hyb_floors = $('#nb_hyb_floors').val();
-            console.log(nb_hyb_floors);
-
-        }
-
-        if( building_type == "hybride") {
-            var nb_hyb_basement = $('#nb_hyb_basement').val();
-            console.log(nb_hyb_basement);
-
-        }
-
-        if( building_type == "hybride") {
-            var nb_hyb_parking = $('#nb_hyb_parking').val();
-            console.log(nb_hyb_parking);
-
-        }
-
-        if( building_type == "hybride") {
-            var nb_hyb_max_pers_level = $('#nb_hyb_max_pers_level').val();
-            console.log(nb_hyb_max_pers_level);
-
-        }
-
-        if( building_type == "hybride") {
-            var nb_hrs_activ_day = $('#nb_hrs_activ_day').val();
-            console.log(nb_hrs_activ_day);
-
-        }
-
-        return 
-    }
-    function compute_data(building_type, collected_data){
         if( building_type == "residential") {
         //    calcul
 
